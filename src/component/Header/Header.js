24d00e052503a1e4a79fcb187e2e97f058991ee7
Imakashkendre <akash.shopisthan@gmail.com>
2024-02-13 13:10:39 +0530

    update Header.js

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useContext, useEffect } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import "./Header.css";
 import OpenTemp from "../OpenTemp/OpenTemp";
 import EditTemp from "../EditTemp/EditTemp";
@@ -50,7 +50,12 @@ const Header = ({ onButtonClick }) => {
   const closeOpenTemplatePopup = () => {
     setOpenTemplatePopup(false);
   };
-
+  const openColorPaletPopup = () => {
+    setOpenColorPaletPopup(true);
+  };
+  const closeColorPaletPopup = () => {
+    setOpenColorPaletPopup(false);
+  };
   const openSaveTemplatePopup = () => {
     setSaveTemplatePopup(true);
   };
@@ -65,6 +70,12 @@ const Header = ({ onButtonClick }) => {
 
   const closeEditTemplatePopup = () => {
     setEditTemplatePopup(false);
+  };
+  const closeCoverPageDesignPopup = () => {
+    setCoverPageDesignPopup(false);
+  };
+  const openCoverPageDesignPopup = () => {
+    setCoverPageDesignPopup(true);
   };
 
   const fileInputRef = useRef(null);
@@ -172,125 +183,154 @@ const Header = ({ onButtonClick }) => {
           <div
             className="main-label cursor-pointer"
             onClick={() => handleMenuClick(0)}
-          ></div>
+          >
+            Edit
+          </div>
           {activeMenu === 0 && (
             <ul
-              className="submenu absolute z-10 bg-white shadow mt-2 w-48 leading-tight"
-              style={{ width: "180px", lineHeight: "12px" }}
+              className="submenu w-36 absolute z-10 bg-white shadow mt-2"
+              style={{
+                lineHeight: "24px",
+                fontSize: "13px",
+                textAlign: "center",
+              }}
             >
-              <li className="py-2 px-4 hover:bg-gray-200 text-xs">
-                Open Inspection
-              </li>
-              <li className="py-2 px-4 hover:bg-gray-200 text-xs">
-                Save Inspection
-              </li>
-              <li className="py-2 px-4 hover:bg-gray-200 text-xs">
-                Open Template
-              </li>
-              <li className="py-2 px-4 hover:bg-gray-200 text-xs">
-                Save Template
-              </li>
+              <li className="hover:bg-gray-200">Open Inspection</li>
+              <li className="hover:bg-gray-200">Save Inspection</li>
+              <li className="hover:bg-gray-200">Open Template</li>
+              <li className="hover:bg-gray-200">Save Template</li>
             </ul>
           )}
         </div>
-        <div>
+        {/* <div>
           <ul>
             <li className="ml-5">Edit</li>
           </ul>
-        </div>
+        </div> */}
         <div>
-          <ul>
+          {/* <ul onClick={internetLogin}>
             <li className="ml-5">Internet</li>
-          </ul>
-        </div>
-        <div>
-          <ul>
-            <li className="ml-5">Report Setting</li>
-          </ul>
-        </div>
-
-        <div className="menu-item relative ml-4">
+            {isInternetLoginPopup && <InternetLogin />}
+          </ul> */}
           <div
-            className="main-label cursor-pointer"
+            className="main-label cursor-pointer ml-4"
             onClick={() => handleMenuClick(1)}
           >
-            General information
+            Internet
           </div>
           {activeMenu === 1 && (
             <ul
-              className="submenu absolute z-10 bg-white shadow mt-2"
-              style={{ width: "180px", lineHeight: "12px" }}
+              className="submenu w-36 left-12 absolute z-10 bg-white shadow mt-2"
+              style={{ fontSize: "13px" }}
+              onClick={setInternetLoginPopup}
             >
-              <Link to="/panel1">
-                <li className="py-2 px-4 hover:bg-gray-200">Wall</li>
-              </Link>
-              <li className="py-2 px-4 hover:bg-gray-200">Batch Add Photos</li>
-              <li className="py-2 px-4 hover:bg-gray-200">Clear All Photos</li>
+              <li
+                className=" hover:bg-gray-200"
+                style={{
+                  height: "2em",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                Internet Login
+              </li>
+              {/* {isInternetLoginPopup && <InternetLogin />} */}
             </ul>
           )}
         </div>
-
+        <div>
+          <div className="menu-item relative ml-4">
+            <div
+              className="main-label cursor-pointer"
+              onClick={() => handleMenuClick(2)}
+            >
+              Report Setting
+            </div>
+            {activeMenu === 2 && (
+              <ul
+                className="submenu w-36 absolute z-10 bg-white shadow mt-2"
+                style={{ lineHeight: "12px", fontSize: "13px" }}
+                onClick={openCoverPageDesignPopup}
+              >
+                {/* <Link to="/coverpagedesigner"> */}
+                <li
+                  className=" hover:bg-gray-200"
+                  style={{
+                    height: "2em",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  Cover Page Designer
+                </li>
+                {/* </Link> */}
+              </ul>
+            )}
+          </div>
+        </div>
         <div className="menu-item relative ml-4">
           <div
             className="main-label cursor-pointer"
-            onClick={() => handleMenuClick(2)}
+            onClick={() => handleMenuClick(3)}
           >
-            Photos
+            General information
           </div>
-          {activeMenu === 2 && (
+          {activeMenu === 3 && (
             <ul
-              className="submenu absolute z-10 bg-white shadow mt-2"
-              style={{ width: "180px", lineHeight: "12px" }}
+              className="submenu w-36 absolute z-10 bg-white shadow mt-2"
+              style={{
+                lineHeight: "24px",
+                fontSize: "13px",
+                textAlign: "Center",
+              }}
             >
-              <Link to="/photoreview">
-                <li className="py-2 px-4 hover:bg-gray-200">
-                  Add Review Photos
-                </li>
+              <Link to="/panel1">
+                <li className=" hover:bg-gray-200">Wall</li>
               </Link>
-              <Link to="/batchaddphotos">
-                <li className="py-2 px-4 hover:bg-gray-200">
-                  Batch Add Photos
-                </li>
-              </Link>
-              <li className="py-2 px-4 hover:bg-gray-200">Clear All Photos</li>
+              <li className=" hover:bg-gray-200">Batch Add Photos</li>
+              <li className=" hover:bg-gray-200">Clear All Photos</li>
             </ul>
           )}
         </div>
-
+        <div className="menu-item relative ml-4">
+          <div
+            className="main-label cursor-pointer"
+            onClick={() => handleMenuClick(4)}
+          >
+            Photos
+          </div>
+          {activeMenu === 4 && (
+            <ul
+              className="submenu w-36  absolute z-10 bg-white shadow mt-2"
+              style={{
+                lineHeight: "34px",
+                fontSize: "13px",
+                textAlign: "Center",
+              }}
+            >
+              <Link to="/photoreview">
+                <li className=" hover:bg-gray-200">Add Review Photos</li>
+              </Link>
+              {/* <Link to="/batchaddphotos"> */}
+              <li
+                className=" hover:bg-gray-200"
+                onClick={setBatchAddPhotosPopup}
+              >
+                Batch Add Photos
+              </li>
+              {/* </Link> */}
+              <li className=" hover:bg-gray-200">Clear All Photos</li>
+            </ul>
+          )}
+        </div>
         <div>
           <ul>
             <li className="ml-5">About</li>
           </ul>
-
         </div>
-        <div>
-          <div>
-            <ul className="flex ml-5 gap-5">
-              {editTempArray.map((edit) => (
-                <li key={edit.id} className="menu-item" onClick={(e) => toggleSubMenu(edit.id, { top: e.clientY, left: e.clientX })}>
-                  {edit.menuname}
-
-
-                  <div>
-                    <ul>
-                      <li onClick={handleLiClick}>
-
-                        <SubmenuPopup isVisible={activeMenu === edit.id} submenuItems={edit.subItems} position={submenuPosition} onClose={closeSubmenu} />
-                      </li>
-                    </ul>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </div>
-
-        </div>
-
-
-
-
       </div>
-
       <div
         className="header text-sm border-b-2 border-black"
         style={{ backgroundColor: "#f7f7f7" }}
@@ -349,11 +389,11 @@ const Header = ({ onButtonClick }) => {
                 className="header2-tag-a"
               >
                 <div className="flex justify-center">
-                  <img src={img4} alt="" />
+                  <img src={img5} alt="" />
                 </div>
                 <div>
-                  Save
-                  <br /> Template
+                  Edit <br />
+                  Comments
                 </div>
               </button>
             </li>
@@ -386,7 +426,6 @@ const Header = ({ onButtonClick }) => {
                 </a>
               </li>
             </Link>
-
             <hr />
             <Link to="/">
               <li className="list-for-header-section-main-nav border-r border-black-900">
@@ -414,7 +453,6 @@ const Header = ({ onButtonClick }) => {
                 </a>
               </li>
             </Link>
-
             <Link to="/contractpage">
               <li className="list-for-header-section-main-nav  border-r border-black-900">
                 <a href="#" className="header2-tag-a">
@@ -447,7 +485,7 @@ const Header = ({ onButtonClick }) => {
                   );
                   setValue(paste);
                 }}
-                // onClick={handlePaste}
+              // onClick={handlePaste}
               >
                 <div className="flex justify-center">
                   <img src={img11} alt="" />
@@ -456,44 +494,47 @@ const Header = ({ onButtonClick }) => {
               </a>
             </li>
             <hr />
-
-            <Link to="/generateReport">
-              <li className="list-for-header-section-main-nav">
-                <a href="#" className="header2-tag-a">
-                  <div className="flex justify-center">
-                    <img src={img12} alt="" />
-                  </div>
-                  <div>
-                    Geneate
-                    <br /> report
-                  </div>
-                </a>
-              </li>
-            </Link>
-            <hr />
-            <Link to="/mobileUpload">
-              <li className="list-for-header-section-main-nav">
-                <a href="#" className="header2-tag-a">
-                  <div className="flex justify-center">
-                    <img src={img15} alt="" />
-                  </div>
-                  <div>
-                    Mobile
-                    <br /> Sync
-                  </div>
-                </a>
-              </li>
-
-            </Link>
-            <li className="list-for-header-section-main-nav  border-r border-black-900">
-              <a href="#" className="header2-tag-a">
+            {/* <Link to="/generateReport"> */}
+            <li className="list-for-header-section-main-nav">
+              <a
+                href="#"
+                className="header2-tag-a"
+                onClick={openColorPaletPopup}
+              >
                 <div className="flex justify-center">
-                  <img src={img13} alt="" />
+                  <img src={img12} alt="" />
                 </div>
                 <div>
-                  Upload
+                  Geneate
                   <br /> report
                 </div>
+              </a>
+            </li>
+            {/* </Link> */}
+            <hr />
+            <li className="list-for-header-section-main-nav">
+              <a href="#" className="header2-tag-a">
+                {/* <div className="flex justify-center">
+                  <img src={img15} alt="" />
+                </div>
+                <div>
+                  Mobile
+                  <br /> Sync
+                </div> */}
+                <Link to="/mobilesync">
+                  <li className="list-for-header-section-main-nav">
+                    <a href="#" className="header2-tag-a p-0">
+                      <div className="flex justify-center">
+                        <img src={img15} alt="" />
+                      </div>
+                      <div>
+                        Mobile
+                        <br />
+                        Sync
+                      </div>
+                    </a>
+                  </li>
+                </Link>
               </a>
             </li>
             <hr />
@@ -505,23 +546,44 @@ const Header = ({ onButtonClick }) => {
             <OpenTemp onClose={closeOpenTemplatePopup} />
           </div>
         )}
-
         {saveTemplatePopup && (
           <div className="popup">
             {/* Render your OpenTemplate component here */}
             <SaveTemp onClose={closeSaveTemplatePopup} />
           </div>
         )}
-
         {editTemplatePopup && (
           <div className="popup m-0">
             {/* Render your EditTemplate component here */}
             <EditTemp onClose={closeEditTemplatePopup} />
+          </div>
+        )}{" "}
+        {internetLoginPopup && (
+          <div className="popup m-0">
+            {/* Render your EditTemplate component here */}
+            <InternetLogin onClose={closeisInternetLoginPopup} />
+          </div>
+        )}
+        {batchAddPhotosPopup && (
+          <div className="popup m-0">
+            {/* Render your EditTemplate component here */}
+            <BatchAddPhotos onClose={closeBatchAddPhotosPopup} />
+          </div>
+        )}{" "}
+        {coverPageDesignPopup && (
+          <div className="popup Cover-Page-Design-Popup-ccc">
+            {/* Render your EditTemplate component here */}
+            <CoverPageDesigner onClose={closeCoverPageDesignPopup} />
+          </div>
+        )}
+        {colorPaletPopup && (
+          <div className="popup Cover-Page-Design-Popup-ccc">
+            {/* Render your color palet component here */}
+            <ColorPalette onClose={closeColorPaletPopup} />
           </div>
         )}
       </div>
     </>
   );
 };
-
 export default Header;
