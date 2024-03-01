8a5d8e1c2c47a612cf936b17394a991e29b72669
amardip padghan <82278378+amardippadghan@users.noreply.github.com>
2024-03-01 13:47:45 +0530

    update Location.js

--- src/component/Photo/Location.js
+++ src/component/Photo/Location.js
@@ -1,207 +1,3 @@
-// import React from "react";
-// import "./Location.css";
-// import Recallcheck from "./Recallcheck";
-// import { Link } from "react-router-dom";
-// const Location = () => {
-//   return (
-//     <>
-//       <div className="container-for-locations-and-recall-component">
-//         <div className="syysysysy">
-//           <div className="PhotoReview-Location-container">
-//             <label htmlfor="username">Location</label>
-//             <br />
-//             <select className="PhotoReview-Location-first-inputfields"></select>
-//             <br />
-//             <select className="PhotoReview-Location-first-inputfields"></select>
-//           </div>
-//           <div className="PhotoReview-Location-button">
-//             <Link to="/panel1">
-//               <button>@</button>
-//             </Link>
-//           </div>
-
-//           <div className="PhotoReview-Location-Checkbox-Container">
-//             <section className="Section-for-label-and-checkbox">
-//               <input type="checkbox" name="agree" />
-//               <label
-//                 htmlfor="agree"
-//                 className="PhotoReview-Location-Checkbox-label"
-//               >
-//                 Print At End
-//               </label>
-//             </section>
-//             <section className="Section-for-label-and-checkbox">
-//               <input
-//                 type="checkbox"
-//                 className="PhotoReview-Location-Checkbox-label"
-//                 name="agree"
-//               />
-//               <label
-//                 htmlfor="agree"
-//                 className="PhotoReview-Location-Checkbox-label"
-//               >
-//                 Summary
-//               </label>
-//             </section>
-//             <section className="Section-for-label-and-checkbox">
-//               <input type="checkbox" id="agree" name="agree" />
-//               <label
-//                 htmlfor="agree"
-//                 className="PhotoReview-Location-Checkbox-label"
-//               >
-//                 Use Location As Caption
-//               </label>
-//             </section>
-//           </div>
-//         </div>
-//         {/* <div className="recalllllll"><Recallcheck /></div> */}
-//       </div>
-//     </>
-//   );
-// };
-// export default Location;
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// import React, { useEffect, useState } from "react";
-// import "./Location.css";
-
-// const Location = () => {
-//   const [menuData, setMenuData] = useState(null);
-//   const [selectedMenuId, setSelectedMenuId] = useState(null);
-//   const [selectedSubmenuId, setSelectedSubmenuId] = useState(null);
-//   const [submenuDetails, setSubmenuDetails] = useState(null);
-
-//   useEffect(() => {
-//     const storedMenuData = localStorage.getItem("menuData");
-//     if (storedMenuData) {
-//       setMenuData(JSON.parse(storedMenuData));
-//     }
-//   }, []);
-
-//   useEffect(() => {
-//     if (selectedSubmenuId && menuData && menuData[selectedMenuId]) {
-//       setSubmenuDetails(menuData[selectedMenuId].subdetails[selectedSubmenuId]);
-//     } else {
-//       setSubmenuDetails(null);
-//     }
-//   }, [selectedSubmenuId, menuData, selectedMenuId]);
-
-//   const handleMenuChange = (e) => {
-//     const selectedId = e.target.value;
-//     setSelectedMenuId(selectedId);
-//     setSelectedSubmenuId(null);
-//   };
-
-//   const handleSubmenuChange = (e) => {
-//     const selectedId = e.target.value;
-//     setSelectedSubmenuId(selectedId);
-//   };
-
-//   return (
-//     <>
-//       <div className="container-for-locations-and-recall-component">
-//         <div className="syysysysy">
-//           <div className="PhotoReview-Location-container">
-//             <label htmlFor="menuDropdown">Location</label>
-//             <div>
-//               <select
-//                 id="menuDropdown"
-//                 className="PhotoReview-Location-first-inputfields"
-//                 onChange={handleMenuChange}
-//               >
-//                 <option value="">Select Menu</option>
-//                 {menuData &&
-//                   Object.keys(menuData).map((menuId) => (
-//                     <option key={menuId} value={menuId}>
-//                       {menuData[menuId].name}
-//                     </option>
-//                   ))}
-//               </select>
-//               <br />
-//               <label htmlFor="submenuDropdown"></label>
-
-//               <select
-//                 id="submenuDropdown"
-//                 className="PhotoReview-Location-first-inputfields"
-//                 onChange={handleSubmenuChange}
-//                 disabled={!selectedMenuId}
-//               >
-//                 <option value="">Select Submenu</option>
-//                 {selectedMenuId &&
-//                   menuData[selectedMenuId].subitems.map((submenu) => (
-//                     <option key={submenu.id} value={submenu.id}>
-//                       {submenu.subName}
-//                     </option>
-//                   ))}
-//               </select>
-
-//               <label htmlFor="damageDropdown"></label>
-//               <br />
-//               <select
-//                 id="damageDropdown"
-//                 className="PhotoReview-Location-first-inputfields"
-//                 disabled={!selectedSubmenuId}
-//               >
-//                 <option value="">Select Damage</option>
-//                 {submenuDetails &&
-//                   Object.values(submenuDetails).map((details) => (
-//                     <option
-//                       key={details.Damage1Data}
-//                       value={details.Damage1Data}
-//                     >
-//                       {details.damage1}
-//                     </option>
-//                   ))}
-//               </select>
-//             </div>
-//             <div className="PhotoReview-Location-button">
-//               {/* <Link to="/panel1"> */}
-//               <button>@</button>
-//               {/* </Link> */}
-//             </div>
-//             <div className="PhotoReview-Location-Checkbox-Container">
-//               <section className="Section-for-label-and-checkbox">
-//                 <input type="checkbox" name="agree" />
-//                 <label
-//                   htmlfor="agree"
-//                   className="PhotoReview-Location-Checkbox-label"
-//                 >
-//                   Print At End
-//                 </label>
-//               </section>
-//               <section className="Section-for-label-and-checkbox">
-//                 <input
-//                   type="checkbox"
-//                   className="PhotoReview-Location-Checkbox-label"
-//                   name="agree"
-//                 />
-//                 <label
-//                   htmlfor="agree"
-//                   className="PhotoReview-Location-Checkbox-label"
-//                 >
-//                   Summary
-//                 </label>
-//               </section>
-//               <section className="Section-for-label-and-checkbox">
-//                 <input type="checkbox" id="agree" name="agree" />
-//                 <label
-//                   htmlfor="agree"
-//                   className="PhotoReview-Location-Checkbox-label"
-//                 >
-//                   Use Location As Caption
-//                 </label>
-//               </section>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default Location;
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 import React, { useEffect, useState } from "react";
 import "./Location.css";
 import { Link } from "react-router-dom"; // Import Link from react-router-dom
@@ -211,6 +7,8 @@ const Location = () => {
   const [selectedMenuId, setSelectedMenuId] = useState(null);
   const [selectedSubmenuId, setSelectedSubmenuId] = useState(null);
   const [submenuDetails, setSubmenuDetails] = useState(null);
+  const [TabData, setTabData] = useState(null);
+  const [panelId, setPanelId] = useState(null);
 
   useEffect(() => {
     const storedMenuData = localStorage.getItem("menuData");
@@ -236,7 +34,25 @@ const Location = () => {
   const handleSubmenuChange = (e) => {
     const selectedId = e.target.value;
     setSelectedSubmenuId(selectedId);
+    setPanelId(null); // Reset panelId when submenu changes
+    // Update submenu details based on selected submenu ID
+    if (
+      menuData &&
+      menuData[selectedMenuId] &&
+      menuData[selectedMenuId].subdetails[selectedId]
+    ) {
+      setSubmenuDetails(menuData[selectedMenuId].subdetails[selectedId]);
+    } else {
+      setSubmenuDetails(null);
+    }
   };
+
+  const handleTabChange = (e) => {
+    const panelId = e.target.value;
+    setPanelId(panelId);
+    setTabData(submenuDetails[panelId]); // Update TabData based on selected panelId
+  };
+
   useEffect(() => {
     console.log("menuData:", menuData);
   }, [menuData]);
@@ -252,6 +68,11 @@ const Location = () => {
   useEffect(() => {
     console.log("submenuDetails:", submenuDetails);
   }, [submenuDetails]);
+
+  useEffect(() => {
+    console.log("panelId:", panelId);
+    console.log("TabData:", TabData);
+  }, [panelId]);
 
   return (
     <>
@@ -284,11 +105,16 @@ const Location = () => {
               >
                 <option value="">Select Submenu</option>
                 {selectedMenuId &&
-                  menuData[selectedMenuId].subitems.map((submenu) => (
-                    <option key={submenu.id} value={submenu.id}>
-                      {submenu.subName}
-                    </option>
-                  ))}
+                  Object.keys(menuData[selectedMenuId].subitems).map(
+                    (subkey) => (
+                      <option
+                        key={subkey}
+                        value={menuData[selectedMenuId].subitems[subkey].si}
+                      >
+                        {menuData[selectedMenuId].subitems[subkey].subName}
+                      </option>
+                    )
+                  )}
               </select>
 
               <label htmlFor="damageDropdown"></label>
@@ -297,17 +123,19 @@ const Location = () => {
                 id="damageDropdown"
                 className="PhotoReview-Location-first-inputfields"
                 disabled={!selectedSubmenuId}
+                onChange={handleTabChange}
               >
                 <option value="">Select Damage</option>
                 {submenuDetails &&
-                  Object.values(submenuDetails).map((details) => (
-                    <option
-                      key={details.Damage1Data}
-                      value={details.Damage1Data}
-                    >
-                      {details.damage1}
-                    </option>
-                  ))}
+                  Object.keys(submenuDetails)
+                    .filter(
+                      (subdetailkey) => !["id", "si"].includes(subdetailkey)
+                    )
+                    .map((subdetailkey) => (
+                      <option key={subdetailkey} value={subdetailkey}>
+                        {submenuDetails[subdetailkey].tabname}
+                      </option>
+                    ))}
               </select>
             </div>
             <div className="PhotoReview-Location-button">
