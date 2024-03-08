5e29ac09c97fce38c6642c7e23d8183384cc0dc5
Bhaktijachavan <bhaktijachavan17@gmail.com>
2024-03-08 10:52:43 +0530

    update PannelComponent.js and SelectionComponent.js

--- src/component/Panel/PannelComponent/PannelComponent.js
+++ src/component/Panel/PannelComponent/PannelComponent.js
@@ -319,7 +319,7 @@ const PannelComponent = ({ showAlternateContent, setRed, setBlack, value }) => {
                 moveDown={handleMoveDownDamage}
               />
 
-              <input />
+             
 
               <div className="p-5">
                 {["Good", "Fair", "Poor", "N/A", "None"].map((label, index) => (
@@ -355,7 +355,7 @@ const PannelComponent = ({ showAlternateContent, setRed, setBlack, value }) => {
               <button
                 onClick={handlesave}
                 type="button"
-                className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+                className="bg-gray-100 border border-gray-400 hover:bg-blue-100 text-black px-10 py-0 rounded"
               >
                 Save Data For report
               </button>
--- src/component/Panel/SelectionComponent/SelectionComponent.js
+++ src/component/Panel/SelectionComponent/SelectionComponent.js
@@ -224,8 +224,8 @@ const SelectionComponent = ({ panelData, value, classname }) => {
             <button
               onClick={handlesave}
               type="button"
-              className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-            >
+              className="bg-gray-100 border border-gray-400 hover:bg-blue-100 text-black py-0 rounded text-sm w-24"
+              >
               Save Data For report
             </button>
           </div>
