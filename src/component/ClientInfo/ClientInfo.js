2e4a4a5ed6514f46f9e083deb997cceb35eabfa8
prasadrahane06 <prasadrahane06@gmail.com>
2024-04-08 12:26:44 +0530

    update ClientInfo.js

--- src/component/ClientInfo/ClientInfo.js
+++ src/component/ClientInfo/ClientInfo.js
@@ -112,20 +112,19 @@ const ClientInfo = () => {
     alert("Client Info Has been clear");
   };
   return (
-    <div className="main-container-clientinfo">
+    <div className="main-container-clientinfo ">
       {pop && (
         <div className="fixed inset-0 flex items-center justify-center z-50">
-          <div className=""></div>
           <div className="relative max-w-md ">
             <div className="popup">
-              <div className="popup-content">
+              <div className="popup-content flex flex-col items-center content-center justify-center">
                 <h2 className="text-xl font-semibold mb-2">Inspection Saved</h2>
-                <p className="text-gray-700">
+                <p className="text-gray-700 text-center">
                   Your inspection has been saved successfully.
                 </p>
                 <button
                   onClick={() => setpop(false)}
-                  className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+                  className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none flex items-center justify-center"
                 >
                   Close
                 </button>
