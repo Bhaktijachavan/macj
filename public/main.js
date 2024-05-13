d10fbbf5bf9b2607e0749935703461528fd38536
akashkendre1298 <kendreakash34@gmail.com>
2024-05-13 16:31:54 +0530

    update 47 files

--- public/main.js
+++ public/main.js
@@ -1,58 +1,3 @@
-const { app, BrowserWindow } = require("electron");
-const path = require("path");
-
-let win;
-
-function createWindow() {
-  win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      contextIsolation: true,
-      preload: path.join(__dirname, "preload.js"),
-      nodeIntegration: true,
-      enableRemoteModule: true,
-    },
-    autoHideMenuBar: true,
-  });
-
-  // const appPath = app.isPackaged ? path.dirname(app.getPath("exe")) : __dirname;
-  // const indexPath = path.join(appPath, "build", "index.html");
-
-  // win.loadURL("file:///D:/virtuebyteprojects/WebApp/macj/public/build/index.html")
-  // win.loadURL("file:///D:/virtuebyteprojects/WebApp/macj/public/build/index.html")
-  win
-    .loadURL("http://localhost:3000")
-
-    .then(() => {
-      console.log("Load successful");
-    })
-    .catch((error) => {
-      console.error("Error loading the URL:", error);
-    });
-
-  // Log additional information
-  win.webContents.on("did-fail-load", (event, errorCode, errorDescription) => {
-    console.error("Failed to load URL:", errorCode, errorDescription);
-  });
-}
-
-app.whenReady().then(() => {
-  createWindow();
-
-  app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
-});
-
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
-});
-
 // const { app, BrowserWindow } = require("electron");
 // const path = require("path");
 
@@ -71,29 +16,14 @@ app.on("window-all-closed", () => {
 //     autoHideMenuBar: true,
 //   });
 
-//   // const appath = app.isPackaged ? path.dirname(app.getPath("exe")) : __dirname;
+//   // const appPath = app.isPackaged ? path.dirname(app.getPath("exe")) : __dirname;
 //   // const indexPath = path.join(appPath, "build", "index.html");
 
 //   // win.loadURL("file:///D:/virtuebyteprojects/WebApp/macj/public/build/index.html")
 //   // win.loadURL("file:///D:/virtuebyteprojects/WebApp/macj/public/build/index.html")
-
-//   //  win.loadURL(`file:///C:/Users/amar/OneDrive/Desktop/virtuebyte/newmacje/macj/public/build/index.html`)
-//   // const filePath =
-//   //   "C:/Users/amar/OneDrive/Desktop/virtuebyte/newmacje/macj/dist/electron-react-win32-x64/resources/app/build/index.html";
-
-//   const baseDirectory = __dirname;
-
-//   // Construct the file path with the correct relative path
-//   const filePath = path.join(baseDirectory, "..", "build", "index.html");
-
-//   // Convert the path to a file URL
-//   const fileUrl = `file:///${filePath}`;
-
-//   console.log("fileUrl", fileUrl);
-
-//   // Load the local file using the dynamically constructed file URL
 //   win
-//     .loadURL(fileUrl)
+//     .loadURL("http://localhost:3000")
+
 //     .then(() => {
 //       console.log("Load successful");
 //     })
@@ -122,3 +52,65 @@ app.on("window-all-closed", () => {
 //     app.quit();
 //   }
 // });
+
+const { app, BrowserWindow, dialog } = require("electron");
+const path = require("path");
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      contextIsolation: true,
+      preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: true,
+      enableRemoteModule: true,
+    },
+    autoHideMenuBar: true,
+  });
+
+  const baseDirectory = __dirname;
+  const filePath = path.join(baseDirectory, "..", "build", "index.html");
+  const fileUrl = `file:///${filePath}`;
+
+  win.loadURL(fileUrl);
+
+  win.webContents.on("did-fail-load", (event, errorCode, errorDescription) => {
+    console.error("Failed to load URL:", errorCode, errorDescription);
+  });
+
+  // Handle close event
+  win.on("close", (event) => {
+    const choice = dialog.showMessageBoxSync(win, {
+      type: "question",
+      buttons: ["Cancel", "OK"],
+      title: "Confirm",
+      message:
+        "Your data will be cleared. Are you sure you want to close the application?",
+    });
+
+    if (choice === 1) {
+      win.webContents.executeJavaScript("localStorage.clear();");
+    } else {
+      event.preventDefault();
+    }
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
